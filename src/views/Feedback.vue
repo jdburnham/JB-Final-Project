d82901bc459011fec3069173e7d810d42cd04864
jdburnham <jburnham@stu.southtexascollege.edu>
2020-02-26 10:57:33 -0600

    Test Google Form on Feedback Page

--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>Feedback Form Page</h1>
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeCAfYNotY5OYgOIN80s3gk1WPPTNwmDucLy469ulDyCTheGQ/viewform?embedded=true"
+      width="640"
+      height="995"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >Loading…</iframe>
   </div>
 </template>
 
@@ -9,6 +16,3 @@ export default {
   name: 'Feedback'
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
